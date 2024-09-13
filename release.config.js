d8dd8e9f90f4a4b28cc243379c11c823cd16d1e8
samchan2022 <samchan.goo.123@gmail.com>
2024-09-13 20:59:25 +0100

    feat: add release.config.js

---
+++ release.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  branches: ["main"],
+  plugins: [
+    "@semantic-release/commit-analyzer", // Analyze commit messages to determine version bump
+    "@semantic-release/release-notes-generator", // Generate release notes
+    "@semantic-release/changelog", // Update changelog file
+    "@semantic-release/npm", // Publish to npm (if applicable)
+    "@semantic-release/github", // Create GitHub releases
+    [
+      "@semantic-release/git", // Commit updated changelog and version files
+      {
+        assets: ["package.json", "CHANGELOG.md"],
+        message: "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
+      }
+    ]
+  ],
+  preset: "conventionalcommits"
+};
+
